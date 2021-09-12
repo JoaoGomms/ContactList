a12333b5eb2b00c920018e194ab3cdafaf1ed502
joaogomes <joaoguilherme102030@gmail.com>
2021-09-12 18:16:57 -0300

    Create new nav graph to handle naviagtion between ListFragment and AddFragment

--- app/src/main/java/com/portifolio/contactlists/data/UserEntity.kt
+++ app/src/main/java/com/portifolio/contactlists/data/UserEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "user", )
+@Entity(tableName = "user")
 data class UserEntity(
 
     @PrimaryKey(autoGenerate = true)
