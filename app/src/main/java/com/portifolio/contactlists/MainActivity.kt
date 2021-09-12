dae83d2f4526fa64d87a4d5c253d63a89f1e4d69
joaogomes <joaoguilherme102030@gmail.com>
2021-09-12 18:19:31 -0300

    Add a FragmentContainerView on MainActivity.kt to handle with fragments using previously created nav

--- app/src/main/java/com/portifolio/contactlists/MainActivity.kt
+++ app/src/main/java/com/portifolio/contactlists/MainActivity.kt
@@ -2,10 +2,32 @@ package com.portifolio.contactlists
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.portifolio.contactlists.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
